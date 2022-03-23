d93ce0da0f66493fc842e4284240b69731227d54
Xu Xing <xing.xu@intel.com>
2022-03-23 17:26:39 +0800

    Add Warn

--- time_notask.js
+++ time_notask.js
@@ -8,12 +8,15 @@ async function loop(max) {
 const LOOP_SIZE = 1000000000;
 async function asyncCall(tag = '') {
   // #1 V8Task
+  console.warn("V8Task #1 Get start time");
   console.log(tag + ' asyncCall start');
   const start = performance.now();
   // #2 V8Task Insert MicroTask
+  console.warn("V8Task #2 Insert MicroTask");
   var result = await loop(LOOP_SIZE);
   const end = performance.now();
   console.log(tag + ' asyncCall time = ' + (end - start));
+  console.warn("V8MicroTask #4 Get end time");
 }
 const start = performance.now();
 loop(LOOP_SIZE);
@@ -22,7 +25,10 @@ console.log(`sync loop (${LOOP_SIZE}) time = ` + (end - start));
 asyncCall("1st: ");
 // If comment out below, the async is correct.
 // #3 V8Task Loop
+console.warn("V8Task #3 Main");
+console.warn("V8Task #3 Main Loop");
 loop(LOOP_SIZE);
+console.warn("V8Task #3 Main end");
 // asyncCall("2nd: ");
 console.log('Program end');
 // #4 MicroTask Loop
