62ef913923abda3ed88c0d5cb0d319c7d7271fbb
Xu Xing <xing.xu@intel.com>
2022-03-24 00:33:24 +0800

    Add settimeout topawait case

---
+++ time_settimeout_topawait.js
@@ -0,0 +1,48 @@
+
+
+const [loopCount, control] = getURLState(location.search);
+
+function resolveAfter2Seconds() {
+  var promise = new Promise(resolve => {
+    console.log('new Promise executor');
+    // const start = performance.now();
+
+    setTimeout(() => {
+      // const end = performance.now();
+      // console.log('setTimeout time = ' + (end - start));
+      resolve('resolved');
+    }, 2000);
+  });
+  // console.log('resolveAfter2Seconds end');
+  return promise;
+}
+
+async function asyncCall() {
+  console.log('asyncCall start');
+  const start = performance.now();
+  var result = await resolveAfter2Seconds();
+  const end = performance.now();
+  console.log('asyncCall time = ' + (end - start));
+  console.log(result);
+  console.log('asyncCall end');
+}
+
+function loop(max) {
+  var sum = 0;
+  for (var i = 0; i < max; i++) {
+    sum = sum + Math.sqrt(i);
+  }
+}
+
+(async function() {
+  const LOOP_SIZE = 1000000000 * loopCount;
+  const start = performance.now();
+  if (control != 1) {
+    loop(LOOP_SIZE);
+  }
+  const end = performance.now();
+  console.log(`sync loop (${LOOP_SIZE}) time = ` + (end - start));
+  await asyncCall();
+  loop(LOOP_SIZE);
+  console.log('Program end');
+})();
