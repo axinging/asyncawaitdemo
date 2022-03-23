322204b0b7da269e7ea5470f364f6b6fe47c31f6
Xu Xing <xing.xu@intel.com>
2022-03-23 16:50:17 +0800

    omments

--- time_notask.js
+++ time_notask.js
@@ -1,14 +1,16 @@
 async function loop(max) {
   var sum = 0;
   for (var i = 0; i < max; i++) {
-    if (i == 100) console.log(i);
+    // if (i == 100) console.log(i);
     sum = sum + Math.sqrt(i);
   }
 }
 const LOOP_SIZE = 1000000000;
 async function asyncCall(tag = '') {
+  // #1 V8Task
   console.log(tag + ' asyncCall start');
   const start = performance.now();
+  // #2 V8Task Insert MicroTask
   var result = await loop(LOOP_SIZE);
   const end = performance.now();
   console.log(tag + ' asyncCall time = ' + (end - start));
@@ -18,8 +20,10 @@ loop(LOOP_SIZE);
 const end = performance.now();
 console.log(`sync loop (${LOOP_SIZE}) time = ` + (end - start));
 asyncCall("1st: ");
-// main loop
 // If comment out below, the async is correct.
+// #3 V8Task Loop
 loop(LOOP_SIZE);
-asyncCall("2nd: ");
+// asyncCall("2nd: ");
 console.log('Program end');
+// #4 MicroTask Loop
+// #5 MicroTask Loop get end time;
