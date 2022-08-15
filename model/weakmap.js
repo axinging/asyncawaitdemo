4d30bf023093b4019461e152d0d7017ee6c92e13
Xu Xing <xing.xu@intel.com>
2022-08-15 13:51:23 +0800

    Add map weakmap

---
+++ model/weakmap.js
@@ -0,0 +1,18 @@
+// https://javascript.plainenglish.io/javascript-finalizer-2859f0832f07
+let arrCollection = new WeakMap()
+
+const registerFinalizer = new FinalizationRegistry(message => {
+  console.log(message)
+});
+
+(function() {
+  let arr = [1, 2, 3]
+  
+  arrCollection.set(arr, "first array")
+ 
+  registerFinalizer.register(arr, 'Array is now cleaned up')
+} ())
+
+setInterval(function() {
+  console.log(arrCollection)
+}, 3000)
