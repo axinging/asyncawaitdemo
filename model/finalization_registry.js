8cdc4f9f0031af4c98c363d62fda319bc7a3510d
Xu Xing <xing.xu@intel.com>
2022-08-15 12:48:17 +0800

    Add finalizer

---
+++ model/finalization_registry.js
@@ -0,0 +1,48 @@
+async function finalization_registry() {
+  const start = tf.memory().numTensors;
+  let waitingForCleanup = true;
+  const startTime = Date.now();
+  const aData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  {
+    console.log('Case will leak 1 tensor');
+
+    const dtype = 'float32';
+    let a = tf.tensor2d(aData, [8, 2], dtype);
+    a = null;
+    let end = tf.memory().numTensors;
+    console.log(
+        'start = ' + start + ', end = ' + end + ', leak = ' + (end - start));
+
+    let b = tf.tensor2d(aData, [8, 2], dtype);
+    end = tf.memory().numTensors;
+    console.log(' end = ' + end + ', leak = ' + (end - start));
+
+    const registry = new FinalizationRegistry((heldValue) => {
+      console.log(`cleanup: ${heldValue}`);
+      console.log(`cleanup was called after ${
+          ((Date.now() - startTime) / 1000).toFixed(1)}s`);
+      waitingForCleanup = false;
+    });
+
+    registry.register(b, 40);
+    b = null;
+  }
+  let end = tf.memory().numTensors;
+  console.log(' end = ' + end + ', leak = ' + (end - start));
+
+
+  console.log('Allocating a lot of objects to try to force garbage collection');
+  while (waitingForCleanup) {
+    for (let i = 0; i < 100; i++) {
+      const x = new Array(100);
+    }
+    await sleep(1000);
+    console.log('wait');
+  }
+  console.log(`foo was reclaimed after ${
+      ((Date.now() - startTime) / 1000).toFixed(1)}s`);
+  end = tf.memory().numTensors;
+  console.log(' end = ' + end + ', leak = ' + (end - start));
+}
+
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
--- model/tensor.js
+++ model/tensor.js
@@ -5,7 +5,7 @@ async function tensorTest() {
     const aData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
     const dtype = 'float32';
     let a = tf.tensor2d(aData, [8, 2], dtype);
-    a = null;
+    a = null; // Leak!
     const end = tf.memory().numTensors;
     console.log(
         'start = ' + start + ', end = ' + end + ', leak = ' + (end - start));
@@ -18,7 +18,7 @@ async function tensorTest() {
     const b =
         new Float32Array([1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]);
     const a = tf.tensor2d(aData, [8, 2], dtype);
-    const result = tf.add(a, tf.tensor2d(b, [8, 2]));
+    const result = tf.add(a, tf.tensor2d(b, [8, 2])); // Leak!
     await result.data();
     result.dispose();
     a.dispose();
@@ -50,7 +50,7 @@ async function tensorTest() {
   {
     console.log('Case scalar leak 3');
     const start = tf.memory().numTensors;
-    const res = tf.add(tf.scalar(1, 'int32'), tf.scalar(true, 'bool'));
+    const res = tf.add(tf.scalar(1, 'int32'), tf.scalar(true, 'bool')); // Leak!
     const end = tf.memory().numTensors;
     console.log(
         'start = ' + start + ', end = ' + end + ', leak = ' + (end - start));
